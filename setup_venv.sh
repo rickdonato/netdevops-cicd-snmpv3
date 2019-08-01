9b9f27550b522a59c9740e730ee669bfd66dc430
packetpimp3 <admin@packetflow.co.uk>
2019-08-01 09:52:32 +0100

    updates

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 VENV_NAME=$1
+ACTION=$2
 
+create_venv() {
 apt-get update -y
 apt-get install python-pip -y
 pip install virtualenv
 
 virtualenv "${VENV_NAME}"
 source "${VENV_NAME}"/bin/activate
+
 if [ "$?" != 0 ]; then
     echo "ERROR: Unable to create virtualenv"
     exit 1
@@ -20,3 +23,15 @@ else
     pip install -r ./requirements.txt
     exit 0
 fi
+}
+
+delete_venv() {
+  source "${VENV_NAME}"/bin/deactivate
+  rm -rf "${VENV_NAME}"
+}
+
+if [ "$ACTION" == "create" ] ; then
+    create_venv
+elif [ "$ACTION" == "delete" ] ; then
+    delete_venv
+}
